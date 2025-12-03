c937c7b5d2c327425bed3a33d025aa00ed7c1ea4
Aiden Woodland <woodlaai@mail.gvsu.edu>
2025-12-02 21:01:33 -0500

    Updated firebase.ts api keys

--- services/firebase.ts
+++ services/firebase.ts
@@ -9,10 +9,13 @@ import {
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAxFxBkBgsWHWfc2qdEwZc0MlxVm2onDWs",
+  authDomain: "syntax-sensei.firebaseapp.com",
+  projectId: "syntax-sensei",
+  storageBucket: "syntax-sensei.firebasestorage.app",
+  messagingSenderId: "809569281591",
+  appId: "1:809569281591:web:1aa41bef3060bf44ff81b2",
+  measurementId: "G-MYJN7MNQ0E"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
