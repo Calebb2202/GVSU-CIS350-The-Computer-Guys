97ef485a76c5a3ce943fd38157e0f2861f4acecb
Aiden Woodland <woodlaai@mail.gvsu.edu>
2025-12-02 20:47:30 -0500

    resolved leaderboard error conflict

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ import { LandingPage } from "./components/LandingPage";
 import { SignInPage } from "./components/SignInPage";
 import { Dashboard } from "./components/Dashboard";
 import { LessonPage } from "./components/LessonPage";
-import { Leaderboard } from "./components/Leaderboard";
 import { Header } from "./components/Header";
 import { UnitPage } from "./components/LessonTrackPage";
 
@@ -41,7 +40,6 @@ const App = () => {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/unit/:unitId" element={<UnitPage />} />
           <Route path="/lessons/:id" element={<LessonPage />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
         </Route>
       </Route>
 
